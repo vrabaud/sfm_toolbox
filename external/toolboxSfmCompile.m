2d84750d3351e6be542737c170232bdb4151c981
vincent.rabaud@gmail.com <vincent.rabaud@gmail.com@d542d68a-273d-11df-97a4-e9f47260eb65>
2011-04-22 07:07:32 +0000

    - no need to build sba.lib
    git-svn-id: http://vincents-structure-from-motion-matlab-toolbox.googlecode.com/svn/trunk@130 d542d68a-273d-11df-97a4-e9f47260eb65

--- external/toolboxSfmCompile.m
+++ external/toolboxSfmCompile.m
@@ -14,8 +14,9 @@ function toolboxSfmCompile(doSba,gccVer)
 %  toolboxCompile()
 %
 % INPUTS
-%  doSba      - [false] if true, it will compile the static library of SBA
-%               as well as the mex file for SBA
+%  doSba      - [false] if true, it will compile the dll projection files
+%               for SBA and that requires matlab to be launched from the
+%               visual c++ terminal on Windows
 %  gccVer     - on Linux, specify your gcc version here (e.g. 4.2 if
 %               gcc-4.2 is a supported compiler on Matlab), which can be
 %               useful if matlab thinks your gcc is too old/new
@@ -44,56 +45,53 @@ savepwd=pwd; cd(fileparts(mfilename('fullpath'))); cd('../');
 dir_external = [ pwd '/external/' ];
 dir_sba = [ dir_external 'sba/'];
 
-% build the SBA static library and the SBA mex file
-if doSba
-  cd(dir_sba);
-  % delete previous object files
-  fileList = {'sba_levmar.o', 'sba_levmar_wrap.o', 'sba_lapack.o', ...
-    'sba_crsm.o', 'sba_chkjac.o', 'libsba.a', 'sba.lib', ...
-    'matlab/sba.mexw32', 'matlab/sba.mexw64'};
-  for i=1:length(fileList)
-    if exist([dir_sba, '/', fileList{i}], 'file'), delete(fileList{i}); end
-  end
-  
-  sba_files = [ 'sba.c ../sba_levmar_wrap.c ../sba_levmar.c ' ...
-    '../sba_lapack.c ../sba_crsm.c ../sba_chkjac.c' ];
-  
-  cd matlab
-  switch computer
-    case {'PCWIN','PCWIN64'},
-      ext = mexext;
-      ext = ext(5:6);
-      library_flags = [ '-D_WIN32 -I' dir_sba ' -L' dir_sba ...
-        ' -L' dir_external 'blasLapack/win' ext ' -output sba.' mexext ' '];
-      % Matlab on Windows 32/64
-      if strcmp(computer, 'PCWIN')
-        eval([ 'mex ' library_flags ...
-          '-lclapack.lib -lblas.lib -llibF77.lib ' ...
-          '-llibI77.lib' sba_files ]);
-      else
-        eval([ 'mex LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:libcmt.lib" ' ...
-          library_flags '-lclapack_nowrap -lf2c -lBLAS_nowrap ' sba_files ]);
-      end
-    case {'GLNX86'},
-      % Matlab on Linux
-      eval([ 'mex ' gcc_extra ' -I../ -O -ldl ' sba_files ...
-        '/usr/lib/liblapack.a /usr/lib/atlas/libblas.a ' ...
-        '/usr/lib/libgfortran.so.3' ]);
-    case {'GLNXA64'},
-      % Matlab on Linux 64
-      eval([ 'mex -lgfortran ' gcc_extra ' -I../ -O -ldl ' sba_files ...
-        ' /usr/lib/liblapack_pic.a ' ...
-        '../../blasLapack/linux64/libf77blas.a ' ...
-        '../../blasLapack/linux64/libatlas.a '
-        ]);
-    case {'i686-pc-linux-gnu', 'i686-unknown-linux-gnu', ...
-        'x86_64-pc-linux-gnu', 'x86_64-unknown-linux-gnu'},
-      % Octave on Linux
-      mkoctfile --mex ./sba.c -I../ -lsba -L../
-  end
-  
-  cd ../../..
+% build the SBA mex file
+cd(dir_sba);
+% delete previous object files
+fileList = {'sba_levmar.o', 'sba_levmar_wrap.o', 'sba_lapack.o', ...
+  'sba_crsm.o', 'sba_chkjac.o', 'libsba.a', 'sba.lib', ...
+  'matlab/sba.mexw32', 'matlab/sba.mexw64'};
+for i=1:length(fileList)
+  if exist([dir_sba, '/', fileList{i}], 'file'), delete(fileList{i}); end
 end
+
+sba_files = [ 'sba.c ../sba_levmar_wrap.c ../sba_levmar.c ' ...
+  '../sba_lapack.c ../sba_crsm.c ../sba_chkjac.c' ];
+
+cd matlab
+switch computer
+  case {'PCWIN','PCWIN64'},
+    ext = mexext;
+    ext = ext(5:6);
+    library_flags = [ '-D_WIN32 -I' dir_sba ' -L' dir_sba ...
+      ' -L' dir_external 'blasLapack/win' ext ' -output sba.' mexext ' '];
+    % Matlab on Windows 32/64
+    if strcmp(computer, 'PCWIN')
+      eval([ 'mex LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:libcmt.lib" ' ...
+        library_flags '-lclapack.lib -lblas.lib -llibF77.lib ' ...
+        '-llibI77.lib' sba_files ]);
+    else
+      eval([ 'mex LINKFLAGS="$LINKFLAGS /NODEFAULTLIB:libcmt.lib" ' ...
+        library_flags '-lclapack_nowrap -lf2c -lBLAS_nowrap ' sba_files ]);
+    end
+  case {'GLNX86'},
+    % Matlab on Linux
+    eval([ 'mex ' gcc_extra ' -I../ -O -ldl ' sba_files ...
+      '/usr/lib/liblapack.a /usr/lib/atlas/libblas.a ' ...
+      '/usr/lib/libgfortran.so.3' ]);
+  case {'GLNXA64'},
+    % Matlab on Linux 64
+    eval([ 'mex -lgfortran ' gcc_extra ' -I../ -O -ldl ' sba_files ...
+      ' /usr/lib/liblapack_pic.a ' ...
+      '../../blasLapack/linux64/libf77blas.a ' ...
+      '../../blasLapack/linux64/libatlas.a '
+      ]);
+  case {'i686-pc-linux-gnu', 'i686-unknown-linux-gnu', ...
+      'x86_64-pc-linux-gnu', 'x86_64-unknown-linux-gnu'},
+    % Octave on Linux
+    mkoctfile --mex ./sba.c -I../ -lsba -L../
+end
+cd ../../..
 
 cd sfm/private/sba
 switch computer
@@ -130,8 +128,6 @@ switch computer
     optsAfter = {lapacklib, blaslib};
   case {'GLNX86', 'GLNXA64'},
     % Matlab on Linux
-    % if you get warnings on linux, you could force the gcc version by
-    % adding those options: 'CXX=g++-4.1' 'CC=g++-4.1' 'LD=g++-4.1'
     opts = { ['CXX=g++-' gccVer] ['CC=g++-' gccVer] ['LD=g++-' ...
       gccVer] '-largeArrayDims' '-l' 'mwlapack' '-l' 'mwblas' '-output'};
   case {'i686-pc-linux-gnu', 'i686-unknown-linux-gnu', ...
